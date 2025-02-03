1498789cc55b5bc24f3dab7366d1a2bad9337d42
Dhanush <iamdhanush02@gmail.com>
2025-02-03 11:19:56 +0530

    project setup

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+import pandas as pd
+from google_play_scraper import app
+
+result = app(
+    'com.nianticlabs.pokemongo',
+    lang='en', # defaults to 'en'
+    country='us' # defaults to 'us'
+)
+
+print(json.dumps(result, indent=4))
